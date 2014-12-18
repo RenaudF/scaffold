e8b3190f30a7bebf7a68ba321e9bdab387f9f8bb
Renaud Fontana <sirgzu@hotmail.com>
2014-12-18 10:54:27 +0000

    now using npm proxy settings to update webdriver

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,11 @@
 git submodule init; git submodule update;
 cd frontend; npm install; bower install; grunt; cd ..;
 cd backend; npm install; grunt; cd ..;
-node node_modules/grunt-protractor-runner/node_modules/protractor/bin/webdriver-manager update; grunt;
+# checking npm global config for proxy settings
+proxy=`sed -n '/proxy=\(.*\)/{p;q;}' ~/.npmrc | sed 's/proxy=//'`
+if [[ -n $proxy ]]
+then
+	proxy="--proxy $proxy"
+fi
+# reusing npm proxy settings for webdriver-manager if necessary
+node node_modules/grunt-protractor-runner/node_modules/protractor/bin/webdriver-manager update $proxy; grunt;
